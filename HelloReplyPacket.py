e5215ab884fcc2fc1c7951299829a04ae7b991e4
Divyapuja <divyapuja.vitonde@gmail.com>
2017-03-28 12:39:27 -0400

    Update HelloReplyPacket.py

--- HelloReplyPacket.py
+++ HelloReplyPacket.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python 2.7
 import socket, sys
 from struct import *
 import netifaces as ni
@@ -25,13 +26,16 @@ from netifaces import *
    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 
 '''
-def toBytes(x):
-    return (int(x,16))
+#Not using this
+#def toBytes(x):
+#    return (int(x,16))
 
-def helloReplyPacket(interface,d_mac,dstip,timestamp):
+def helloReplyPacket(interface,d_mac,dstip,timestamp): #while calling d_mac and dstip are set as src_mac and src_ip
     if interface not in ni.interfaces():
         return None
-
+    
+    #Tracing this with pdb 
+    pdb.st_trace()
     S_MAC = ni.ifaddresses(interface)[AF_LINK][0]['addr'].split(":")
     S_MAC = pack('!6c',chr(int(S_MAC[0], 16)), chr(int(S_MAC[1],16)),  chr(int(S_MAC[2],16)) , chr(int(S_MAC[3],16)), chr(int(S_MAC[4],16)), chr(int(S_MAC[5],16)))
 
@@ -62,9 +66,10 @@ def helloReplyPacket(interface,d_mac,dstip,timestamp):
 
 
     ip_header = ip_v_ihl + ip_tos + ip_tot_len + ip_id + ip_frag_off + ip_ttl + ip_proto + ip_check + ip_src + ip_dst
-
+    #dsdv type set as 2 for to indicate its a helloReply packet 
     dsdv_type = pack('!c',chr(2))
     timestamp = pack('!3c',timestamp[0],timestamp[1],timestamp[2])
+    #Where this will return to listen socket or at main here?
     return ethernet_header+ip_header+dsdv_type+timestamp
 
 
@@ -73,11 +78,12 @@ def helloReplyPacket(interface,d_mac,dstip,timestamp):
 def main():
 
     interface = sys.argv[1]
-    d_mac = 'ff:ff:ff:ff:ff:ff'
-    ip_dst = '224.0.0.4'
+    #d_mac = 'ff:ff:ff:ff:ff:ff'
+    #ip_dst = '224.0.0.4'
     s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW)
     s.bind((interface,0))
-    s.send(helloReplyPacket(interface,d_mac,ip_dst))
+    #s.send(helloReplyPacket(interface,d_mac,ip_dst))
+    s.send(helloReplyPacket(interface,d_mac,ip_dst,timestamp))
 
 if __name__ == "__main__":
     main()
